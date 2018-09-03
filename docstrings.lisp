5e0100806f9a13ecbe62446ef717f8eb4dd9974e
Eric Schulte <eschulte@grammatech.com>
2018-09-03 07:41:54 -0400

    Include AST differencing in the manual

--- docstrings.lisp
+++ docstrings.lisp
@@ -471,7 +471,8 @@ with #\@. Optionally downcase the result."
 (defun unescape-for-texinfo (string &aux last-char)
   "Unescape obvious texinfo commands in STRING.
 STRING is assumed to be the result of `escape-for-texinfo'."
-  (let ((w/o-braces '("section" "subsection" "subsubsection" "cindex" "node" "include"))
+  (let ((w/o-braces '("section" "subsection" "subsubsection" "cindex" "node"
+                      "include" "end" "menu"))
         (w/braces '("uref" "ref" "url" "code"))
         (braces 0))
     (flet ((subword-at (i word)
@@ -514,7 +515,7 @@ STRING is assumed to be the result of `escape-for-texinfo'."
 ;;; line markups
 
 (defvar *not-symbols*
-  '("ANSI" "CLHS" "UNIX" "SBCL" "POSIX" "ISO" "ASM" "C" "C++"))
+  '("ANSI" "CLHS" "UNIX" "SBCL" "POSIX" "ISO" "ASM" "C" "C++" "AST"))
 
 (defun frob-ellipsis (line)
   ;; READ-FROM-STRING chokes on ... so replace it.
